d2deb259924efd26de38fb297d44f0dc6e31c97b
pavankumar0077 <40380941+pavankumar0077@users.noreply.github.com>
2022-01-27 15:36:35 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+#! c:\python39\bin
+
+print("Hello World")
+
+for i in range(1,6):
+  for k in range(7, i, -i):
+    print(" ", end = "")
+  for j in range(1, i+1):
+    print(j, end =" ")
+print()
+
